168c31f79864b47f96a96b6133787b1af06cb79c
Behemouth <Behemouth@users.noreply.github.com>
2015-03-11 13:52:59 +0800

    Fixed:#1

--- public/index.php
+++ public/index.php
@@ -6,8 +6,8 @@ require 'main.inc';
 // Send this early, to prevent caching error pages for longer than the duration.
 header(
     'Cache-Control: ' . getCacheControlHeader(
-        Conf::$default_cache_control_max_age, 
-        Conf::$default_cache_control_stale_while_revalidate, 
+        Conf::$default_cache_control_max_age,
+        Conf::$default_cache_control_stale_while_revalidate,
         Conf::$default_cache_control_stale_if_error));
 
 // Make sure to send these security headers are included in all responses.
@@ -25,20 +25,20 @@ foreach ($required_security_headers as $key => $value) {
 Log::add($_SERVER, '$_SERVER');
 
 if (isset($_GET[RedirectWhenBlockedFull::QUERY_STRING_PARAM_NAME])) {
-    
+
     // Key cannot be empty.
     if (Conf::$alt_base_urls_key) {
-        
+
         // Verify key. Set this in conf-local.inc.
         if (isset($_GET['key']) && $_GET['key'] == Conf::$alt_base_urls_key) {
-            
+
             if ($_GET[RedirectWhenBlockedFull::QUERY_STRING_PARAM_NAME] ==
                  Conf::OUTPUT_TYPE_ALT_BASE_URLS) {
                 header('Content-Type: application/javascript');
                 print json_encode(RedirectWhenBlockedFull::getAltBaseUrls());
                 exit();
             }
-            
+
             if ($_GET[RedirectWhenBlockedFull::QUERY_STRING_PARAM_NAME] ==
                  Conf::OUTPUT_TYPE_APK_URLS) {
                 header('Content-Type: application/javascript');
@@ -52,13 +52,13 @@ if (isset($_GET[RedirectWhenBlockedFull::QUERY_STRING_PARAM_NAME])) {
                 print json_encode($urls);
                 exit();
             }
-            
+
             if ($_GET[RedirectWhenBlockedFull::QUERY_STRING_PARAM_NAME] ==
                  Conf::OUTPUT_TYPE_STATUS) {
                 header('Cache-Control: max-age=0');
                 header('Content-Type: text/plain');
                 require 'status_tests/StatusTest.inc';
-                
+
                 foreach (scandir('status_tests/enabled') as $file) {
                     if ($file[0] == '.') {
                         continue;
@@ -68,21 +68,21 @@ if (isset($_GET[RedirectWhenBlockedFull::QUERY_STRING_PARAM_NAME])) {
                     $test = new $class();
                     $tests[] = $test;
                 }
-                
+
                 foreach ($tests as $test) {
                     if (! $test->passed()) {
                         http_response_code(503);
                         break;
                     }
                 }
-                
+
                 foreach ($tests as $i => $test) {
                     if ($i > 0) {
                         print "\n---\n";
                     }
                     print $test;
                 }
-                
+
                 exit();
             }
         }
@@ -96,7 +96,7 @@ if ($request->getUrlComponent('path') == '/crossdomain.xml' &&
      getDownstreamOrigin()) {
     header('Content-Type: application/xml');
     $downstream_origin = getDownstreamOrigin();
-    print 
+    print
         <<<EOF
 <?xml version="1.0" ?>
 <cross-domain-policy>
@@ -122,32 +122,21 @@ $response = new ProxyHttpResponse($client->getResponse(), $request);
 $body = $response->getBody();
 $headers = $response->getHeaders();
 
-// Default - can be overriden below.
-$headers['X-Frame-Options'] = 'SAMEORIGIN';
-
-if (getDownstreamOrigin()) {
-    $headers['Access-Control-Allow-Origin'] = getDownstreamOrigin();
-    
-    // See http://stackoverflow.com/questions/12409600/error-request-header-field-content-type-is-not-allowed-by-access-control-allow.
-    $headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept';
-
-    $headers['X-Frame-Options'] = 'ALLOW-FROM ' . getDownstreamOrigin();
-}
 
 header($response->getResponseInfo());
 foreach ($headers as $key => $values) {
-    
+
     // Don't overwrite security headers.
     if (isset($required_security_headers[$key])) {
         continue;
     }
-    
+
     if (! is_array(($values))) {
         $values = array(
             $values
         );
     }
-    
+
     foreach ($values as $i => $value) {
         header($key . ': ' . $value, ($i == 0));
     }
