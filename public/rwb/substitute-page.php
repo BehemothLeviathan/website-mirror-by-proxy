6f3f4980b7ac3c532c7eea097134a33fe24d08c8
Behemouth <Behemouth@users.noreply.github.com>
2015-06-29 16:05:48 +0800

    Fixed:#19 Share button

--- public/rwb/substitute-page.php
+++ public/rwb/substitute-page.php
@@ -80,6 +80,13 @@
         <li><a href="<?php print $alt_url_collection; ?>" target="_blank"><?php print $alt_url_collection; ?></a></li>
         <?php } ?>
       </ul>
+
+<!-- JiaThis Button BEGIN -->
+<script type="text/javascript" src="./http://v3.jiathis.com/code/jiathis_r.js?move=0&amp;btn=r4.gif" charset="utf-8"></script>
+<!-- JiaThis Button END -->
+
+
+
       <?php } ?>
       <p class="warning" id="rwbTip_JUST_MAGIC_BIT_warning"></p>
       <p class="loading"><img src="<?php print $rwb_path_relative_to_request_path ?>/spinner.gif" alt="Page Loading" /></p>
